3b3504e91fb858e6c9fabf938a3ff142ec5cfe33
KlinketStudios <KlinketStudios@gmail.com>
2026-04-20 09:57:47 -0700

    added knife

---
+++ Assets/Scripts/Knife.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class Knife : MonoBehaviour, IInteractable, IPickupAndPlaceable
+{
+    private IClickListener clickListener;
+    [SerializeField] private Transform origin;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        ThisObject = gameObject;
+        
+        BoxCollider collider = spriteRenderer.GetComponent<BoxCollider>();
+        collider.size = spriteRenderer.sprite.bounds.size;
+    }
+
+    public IClickListener ClickListener
+    {
+        get => clickListener;
+        set => clickListener = value;
+    }
+
+    public void Interacted(GrabHand grabHand)
+    {
+        GetComponent<IPickupAndPlaceable>().PickUp(grabHand);
+    }
+
+    public Transform Origin
+    {
+        get => origin;
+        set => origin = value;
+    }
+
+    public GameObject ThisObject { get; set; }
+    public int OldLayer { get; set; }
+    public Vector3 OldLocalScale { get; set; }
+
+    public void Grabbed()
+    {
+        spriteRenderer.enabled = false;
+    }
+
+    public void Placed()
+    {
+        spriteRenderer.enabled = true;
+    }
+}
